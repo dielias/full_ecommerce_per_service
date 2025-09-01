5ae99610f9f7bddc935a01597c7a1356f242e437
Dinah <dinah.navas@gmail.com>
2025-08-31 22:11:03 -0300

    correção locustfile - criação de order - quantity

--- stress_tests/locustfile.py
+++ stress_tests/locustfile.py
@@ -1,5 +1,6 @@
 from locust import HttpUser, task, between
 import random
+import os
 import uuid
 
 class EcommerceUser(HttpUser):
@@ -15,19 +16,30 @@ class EcommerceUser(HttpUser):
     orders_url = "http://localhost:8477"
 
     def on_start(self):
-        # Criar usuário via proxy do ToxiProxy
-        name = f"User{random.randint(1, 100000)}"
-        email = f"user_{uuid.uuid4()}@test.com"
-        response = self.client.post(f"{self.users_url}/users", json={"name": name, "email": email})
-        if response.status_code in (200, 201):
-            self.user_id = response.json().get("id")
+        try:
+            # Criar usuário via proxy do ToxiProxy
+            name = f"User{random.randint(1, 100000)}"
+            email = f"user_{uuid.uuid4()}@test.com"
+            response = self.client.post(f"{self.users_url}/users", json={"name": name, "email": email})
+            if response.status_code in (200, 201):
+                self.user_id = response.json().get("id")
+            else:
+                print(f"Failed to create user: Status {response.status_code}")
 
-        # Criar produto via proxy do ToxiProxy
-        name = f"Product{random.randint(1, 100000)}"
-        price = round(random.uniform(10.0, 100.0), 2)
-        response = self.client.post(f"{self.products_url}/products", json={"name": name, "price": price})
-        if response.status_code in (200, 201):
-            self.product_id = response.json().get("id")
+            # Criar produto via proxy do ToxiProxy
+            name = f"Product{random.randint(1, 100000)}"
+            price = round(random.uniform(10.0, 100.0), 2)
+            quantity = random.randint(1, 100)
+            response = self.client.post(
+                f"{self.products_url}/products", 
+                json={"name": name, "price": price,"quantity": quantity})
+            if response.status_code in (200, 201):
+                self.product_id = response.json().get("id")
+            else:
+                print(f"Failed to create product: Status {response.status_code}")
+
+        except Exception as e:
+                print(f"Exception during create_order: {e}")
 
     @task(4)
     def create_order(self):
@@ -35,32 +47,33 @@ class EcommerceUser(HttpUser):
             try:
                 response = self.client.post(
                     f"{self.orders_url}/orders",
-                    json={"user_id": self.user_id, "product_id": self.product_id},
-                    timeout=10
+                    json={"user_id": self.user_id, "product_id": self.product_id}
                 )
                 if response.status_code in (200, 201):
                     self.order_id = response.json().get("id")
+                else:
+                    print(f"Failed to create order: Status {response.status_code}")
             except Exception as e:
-                print(f"Falha ao criar pedido: {e}")
+                print(f"Exception during create_order: {e}")
 
     @task(2)
     def list_orders(self):
         try:
-            self.client.get(f"{self.orders_url}/orders", timeout=10)
+            self.client.get(f"{self.orders_url}/orders")
         except Exception as e:
             print(f"Falha ao listar pedidos: {e}")
 
     @task(1)
     def list_users(self):
         try:
-            self.client.get(f"{self.users_url}/users", timeout=10)
+            self.client.get(f"{self.users_url}/users")
         except Exception as e:
             print(f"Falha ao listar usuários: {e}")
 
     @task(1)
     def list_products(self):
         try:
-            self.client.get(f"{self.products_url}/products", timeout=10)
+            self.client.get(f"{self.products_url}/products")
         except Exception as e:
             print(f"Falha ao listar produtos: {e}")
 
@@ -69,15 +82,24 @@ class EcommerceUser(HttpUser):
         name = f"User{random.randint(1, 100000)}"
         email = f"user_{uuid.uuid4()}@test.com"
         try:
-            self.client.post(f"{self.users_url}/users", json={"name": name, "email": email}, timeout=10)
+            response = self.client.post(f"{self.users_url}/users", json={"name": name, "email": email})
+            if response.status_code not in (200, 201):
+                print(f"Failed to create user: Status {response.status_code}")
         except Exception as e:
-            print(f"Falha ao criar usuário: {e}")
+            print(f"Exception during create_user: {e}")
 
     @task(1)
     def create_product(self):
         name = f"Product{random.randint(1, 100000)}"
         price = round(random.uniform(10.0, 100.0), 2)
+        quantity = random.randint(1, 100)
+
         try:
-            self.client.post(f"{self.products_url}/products", json={"name": name, "price": price}, timeout=10)
+            response = self.client.post(
+                f"{self.products_url}/products", 
+                json={"name": name, "price": price, "quantity": quantity}
+                )
+            if response.status_code not in (200, 201):
+                print(f"Failed to create product: Status {response.status_code}")
         except Exception as e:
-            print(f"Falha ao criar produto: {e}")
+            print(f"Exception during create_product: {e}")
